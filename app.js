983e95eb5bf68eac5905203e605bb1801a1e0712
Bruno Ventura <brun0ventur4@gmail.com>
2016-02-29 21:25:00 -0300

    extract configs
    extract configs
    
    Conflicts:
    	app.js

--- app.js
+++ app.js
@@ -3,44 +3,71 @@
 const irc = require('irc');
 const request = require('request');
 const cheerio = require('cheerio');
+const fs = require('fs');
+const NODE_ENV = process.env.NODE_ENV || 'development';
+let config = loadEnv();
+console.log(config);
 
-const client = new irc.Client(
-    'rajaniemi.freenode.net',
-    'ctfbot',
-    { channels: ['#ctf-br'] }
-);
+// const client = new irc.Client(
+//     'rajaniemi.freenode.net',
+//     'ctfbot',
+//     { channels: ['#ctf-bot-test'] }
+// );
 
 const messagesListner = {};
+
+function loadEnv() {
+    try {
+        const file = NODE_ENV != 'production' ? 'config.json': 'dev-config.json'
+        return JSON.parse(fs.readFileSync(file));
+    } catch (err) {
+        console.error(err);
+        process.exit();
+    }
+}
 
 //Generic message handler
 function respondMessage(message, idle, action) {
     messagesListner[message] = {idle, action};
 }
 
-//Magic happens here
+//Listeners
+
+//Message Listener
 client.addListener('message', function (from, to, message) {
     if (messagesListner[message]) {
         const msg = messagesListner[message];
         if (msg.idle > (new Date().getTime() - msg.timer)) {
-            client.say('#ctf-br', `To ocupado!!!`);
+            client.say('#ctf-bot-test', `To ocupado!!!`);
             return;
         }
         msg.timer = new Date().getTime();
         msg.action();
     }
 });
+//Give voice for new members join when they join
+client.addListener('join', function(channel, who){
+	if(who !== 'ctfbot'){
+		client.send('MODE', channel, '+v', who);
+	}
+});
+//Error handler
+client.addListener('error', function(error){
+	console.log(error);
+});
+
 
 //Message handlers
 respondMessage('!nextctf', 10000, () => {
     upcomingCtf()
         .then(ctf => {
-            client.say('#ctf-br', `Nome: ${ctf.name}`)
-            client.say('#ctf-br', `Data: ${ctf.date}`)
-            client.say('#ctf-br', `Tipo: ${ctf.type}`)
-            client.say('#ctf-br', `Site: ${ctf.link}`)
+            client.say('#ctf-bot-test', `Nome: ${ctf.name}`)
+            client.say('#ctf-bot-test', `Data: ${ctf.date}`)
+            client.say('#ctf-bot-test', `Tipo: ${ctf.type}`)
+            client.say('#ctf-bot-test', `Site: ${ctf.link}`)
         })
         .catch(err => {
-            client.say('#ctf-br', `${err}`);
+            client.say('#ctf-bot-test', `${err}`);
         });
 });
 
